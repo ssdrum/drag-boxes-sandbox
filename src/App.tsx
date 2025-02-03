c04beb88bf8f2c76f63a4e9760292694e446b63c
Luigi Di Paolo <luigi.dipaolo02@gmail.com>
2025-02-03 19:20:22 +0000

    Change hardcoded ids to uuids for boxes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import "./App.css";
 import Canvas from "./Canvas";
 import { Group as GroupType } from "./types";
 import { BOX_HEIGHT, BOX_WIDTH } from "./consts";
+import { v4 as uuidv4 } from "uuid";
 
 const initialGroups: GroupType[] = [
   {
-    id: "g-1",
+    id: uuidv4(),
     topSnapPoint: { x: BOX_WIDTH / 2, y: 0 },
     bottomSnapPoint: { x: BOX_WIDTH / 2, y: BOX_HEIGHT },
     children: [
       {
-        id: "g-1-c-1",
+        id: uuidv4(),
         coords: { x: 0, y: 0 },
         bg: "#4A90E2",
         showSnapPreviewUp: true,
@@ -20,26 +21,26 @@ const initialGroups: GroupType[] = [
     ],
   },
   {
-    id: "g-2",
+    id: uuidv4(),
     topSnapPoint: { x: 300 + BOX_WIDTH / 2, y: 300 },
     bottomSnapPoint: { x: 300 + BOX_WIDTH / 2, y: 300 + BOX_HEIGHT * 3 },
     children: [
       {
-        id: "g-2-c-1",
+        id: uuidv4(),
         coords: { x: 300, y: 300 },
         bg: "#90EE90",
         showSnapPreviewUp: false,
         showSnapPreviewDown: false,
       },
       {
-        id: "g-2-c-2",
+        id: uuidv4(),
         coords: { x: 300, y: 350 },
         bg: "#FFD700",
         showSnapPreviewUp: false,
         showSnapPreviewDown: false,
       },
       {
-        id: "g-2-c-3",
+        id: uuidv4(),
         coords: { x: 300, y: 400 },
         bg: "#FF4040",
         showSnapPreviewUp: false,
